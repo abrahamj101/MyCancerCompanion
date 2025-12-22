3e5a5e757e0820623692a93727c70d8419e8bf99
Joshua Abraham <141776519+abrahamj101@users.noreply.github.com>
2025-12-22 00:37:55 -0600

    Move Firebase Config to Environment Variables

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -4,13 +4,14 @@ import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBuQkI6CtRfR8Ivu3KPXPtU6KvprrdcmzA",
-    authDomain: "mycancercompanion-1.firebaseapp.com",
-    projectId: "mycancercompanion-1",
-    storageBucket: "mycancercompanion-1.firebasestorage.app",
-    messagingSenderId: "503966516946",
-    appId: "1:503966516946:web:f96854cefb4b82506d4f0b"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
